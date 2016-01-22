82bb57477b2ff72dba3e83cb2af0499a1f822235
Horatio <horatiorosa@aol.com>
2016-01-22 15:32:57 -0500

    added devise model and views for Students

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+
+
+
+
+
+
+
+
+  devise_for :students
+  
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
