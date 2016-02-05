e7b11ee53db904adecbd0deb96b2686d78a33e0f
Mai Tamakoshi <mai23coco@gmail.com>
2016-02-05 00:36:52 -0500

    created search bar on landing page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,7 @@
 //= require jquery_ujs
 //= require_tree .
 
-// init Masonry after all images have loaded
+// == init Masonry after all images have loaded ==//
 var $grid = $('.grid').imagesLoaded( function() {
   $grid.masonry({
     itemSelector: '.grid-item',
@@ -22,3 +22,66 @@ var $grid = $('.grid').imagesLoaded( function() {
     columnWidth: '.grid-sizer'
   }); 
 });
+
+
+//=== waypoints ===//
+// $(document).ready(function() {
+// //nav bar will be fixed on top after scrolling down below 110px from the top of webpage//
+
+// 	$(window).scroll(function() {
+// 	   var navHeight = $( window ).height() - 110;
+// 			 if ($(window).scrollTop() > navHeight) {
+// 				 $('nav').addClass('fixed');
+// 			 }
+// 			 else {
+// 				 $('nav').removeClass('fixed');
+// 			 }
+// 		});
+// using waypoints plugin//
+
+$('.screenNi').waypoint(function(direction) {
+	if(direction == 'down') {
+	  $('.prf').addClass('animated fadeInLeft pimg');
+ 	  $('.introduction').addClass('animated fadeInRight intro description vis');
+ 	  $('#screen2 .inv').removeClass('inv');
+	} else {
+	  $('.prf').removeClass('animated fadeInLeft pimg');
+	  $('.introduction').removeClass('animated fadeInRight intro description vis');
+	  $('#screen2 .vis').addClass('inv');
+	}
+   },{offset:'70%'});
+
+
+//  $('.screenSan').waypoint(function(direction) {
+//  	if(direction == 'down') {
+//  	 $('.sites').addClass('animated slideInUp');
+// 	 $('#screen3 .inv').removeClass('inv');
+//   } else {
+//   	 $('.sites').removeClass('animated slideInUp');
+// 	 $('#screen3 .vis').addClass('inv');
+//   }
+//  },{offset:'70%'});
+
+//  $('.screenYon').waypoint(function(direction) {
+//  	if(direction == 'down') {
+//  	  $('.icons').addClass('animated fadeInRight');
+// 	  $('object').addClass('animated fadeInLeft');
+// 	 $('#screen4 .inv').removeClass('inv');
+//    } else {
+//    	  $('.icons').removeClass('animated fadeInRight');
+// 	  $('object').removeClass('animated fadeInLeft');
+// 	  $('#screen4 .vis').addClass('inv');
+//    }
+//   },{offset:'70%'});
+
+
+//  $('.screenGo').waypoint(function(direction) {
+//    if(direction =='down') {
+//  	 $('.contactIcons').addClass('animated swing');
+//   } else {
+//   	$('.contactIcons').removeClass('animated swing');
+//   } 
+// },{offset:'80%'});
+
+
+// });  //top
